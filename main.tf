1b0e9d281205919ad280665ec4b33f731ec0180e
venkat <venkat.mobiqa@gmail.com>
2023-03-10 10:33:24 +0530

    updating ./tf-module-app

--- main.tf
+++ main.tf
@@ -1,64 +1,64 @@
-#resource "aws_iam_role" "role" {
-#  name = "${var.env}-${var.component}-role"
-#
-#  assume_role_policy = jsonencode({
-#    Version = "2012-10-17"
-#    Statement = [
-#      {
-#        Action = "sts:AssumeRole"
-#        Effect = "Allow"
-#        Sid    = ""
-#        Principal = {
-#          Service = "ec2.amazonaws.com"
-#        }
-#      },
-#    ]
-#  })
-#
-#  tags = merge(
-#    local.common_tags,
-#    { Name = "${var.env}-${var.component}-role" }
-#  )
-#}
-#
-#resource "aws_iam_instance_profile" "profile" {
-#  name = "${var.env}-${var.component}-role"
-#  role = aws_iam_role.role.name
-#}
-#
-#resource "aws_iam_policy" "policy" {
-#  name        = "${var.env}-${var.component}-parameter-store-policy"
-#  path        = "/"
-#  description = "${var.env}-${var.component}-parameter-store-policy"
-#
-#  policy = jsonencode({
-#    "Version" : "2012-10-17",
-#    "Statement" : [
-#      {
-#        "Sid" : "VisualEditor0",
-#        "Effect" : "Allow",
-#        "Action" : [
-#          "ssm:GetParameterHistory",
-#          "ssm:GetParametersByPath",
-#          "ssm:GetParameters",
-#          "ssm:GetParameter"
-#        ],
-#        "Resource" : "arn:aws:ssm:us-east-1:633788536644:parameter/${var.env}.${var.component}*"
-#      },
-#      {
-#        "Sid" : "VisualEditor1",
-#        "Effect" : "Allow",
-#        "Action" : "ssm:DescribeParameters",
-#        "Resource" : "*"
-#      }
-#    ]
-#  })
-#}
-#
-#resource "aws_iam_role_policy_attachment" "role-attach" {
-#  role       = aws_iam_role.role.name
-#  policy_arn = aws_iam_policy.policy.arn
-#}
+resource "aws_iam_role" "role" {
+  name = "${var.env}-${var.component}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-${var.component}-role" }
+  )
+}
+
+resource "aws_iam_instance_profile" "profile" {
+  name = "${var.env}-${var.component}-role"
+  role = aws_iam_role.role.name
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "${var.env}-${var.component}-parameter-store-policy"
+  path        = "/"
+  description = "${var.env}-${var.component}-parameter-store-policy"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "ssm:GetParameterHistory",
+          "ssm:GetParametersByPath",
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        "Resource" : "arn:aws:ssm:us-east-1:633788536644:parameter/${var.env}.${var.component}*"
+      },
+      {
+        "Sid" : "VisualEditor1",
+        "Effect" : "Allow",
+        "Action" : "ssm:DescribeParameters",
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "role-attach" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
 #
 resource "aws_security_group" "main" {
   name        = "${var.env}-${var.component}-security-group"
@@ -99,9 +99,9 @@ resource "aws_launch_template" "main" {
   image_id               = data.aws_ami.centos8.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
-#  iam_instance_profile {
-#    arn = aws_iam_instance_profile.profile.arn
-#  }
+  iam_instance_profile {
+    arn = aws_iam_instance_profile.profile.arn
+  }
 }
 
 
